cb128bda93008b20c6c8c20ac40ad7d9f99d51a8
ritanshu821 <ritanshu@bcoder.co.in>
2025-09-04 23:00:11 +0530

    inti

--- lib/pages/task_register_page.dart
+++ lib/pages/task_register_page.dart
@@ -15,6 +15,7 @@ class TaskRegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
